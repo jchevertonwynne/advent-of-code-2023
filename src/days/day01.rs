14ab40172341b1dba4712aef3683d08a1fc67c07
Joseph Cheverton-Wynne <jchevertonwynne@gmail.com>
2023-12-02 15:02:22 +0000

    rewritten day 1

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -1,6 +1,4 @@
-use anyhow::Context;
 use bstr::ByteSlice;
-use nom::FindSubstring;
 
 use crate::{DayResult, IntoDayResult};
 
@@ -9,63 +7,93 @@ pub fn solve(input: &str) -> anyhow::Result<DayResult> {
     let mut p2 = 0;
 
     for line in input.as_bytes().lines() {
-        let (first, last) =
-            line.bytes()
-                .enumerate()
-                .fold((None, None), |(mut first, mut last), (index, c)| {
-                    if c.is_ascii_digit() {
-                        let number = c as u32 - '0' as u32;
-                        first = first.or(Some((number, index)));
-                        last = Some((number, index));
-                    }
-                    (first, last)
-                });
-
-        let first1 = first.context("expected a first value")?;
-        let last1 = last.context("expected a last value")?;
-
-        let num1 = (first1.0 * 10) + last1.0;
+        let (p1f, p2f) = first_forward(line);
+        let (p1b, p2b) = first_backward(line);
+        let num1 = p1f * 10 + p1b;
+        let num2 = p2f * 10 + p2b;
         p1 += num1;
-
-        const NUMS: [&str; 9] = [
-            "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
-        ];
-        let first_indices = NUMS.map(|n| line.as_bytes().find_substring(n));
-        let last_indices: [_; 9] = std::array::from_fn(|i| {
-            first_indices[i].and_then(|_| {
-                last.map(|(_, i)| i + 3 < line.len())
-                    .unwrap_or(true)
-                    .then(|| line.as_bytes().rfind(NUMS[i]))
-                    .flatten()
-            })
-        });
-
-        let (first, last) =
-            itertools::izip!(first_indices.into_iter(), last_indices.into_iter(), 1..).fold(
-                (first, last),
-                |(mut first, mut last), (first_match_index, last_match_index, number)| {
-                    if let Some(index) = first_match_index {
-                        first = first
-                            .filter(|f: &(_, _)| f.1 < index)
-                            .or(Some((number, index)));
-                    }
-                    if let Some(index) = last_match_index {
-                        last = last
-                            .filter(|f: &(_, _)| f.1 > index)
-                            .or(Some((number, index)));
-                    }
-                    (first, last)
-                },
-            );
-
-        let first2 = first.context("expected a first value")?;
-        let last2 = last.context("expected a last value")?;
-
-        let num2 = (first2.0 * 10) + last2.0;
         p2 += num2;
     }
 
     (p1, p2).into_result()
+}
+
+fn first_forward(line: &[u8]) -> (usize, usize) {
+    const NUMS_FORWARD: [&str; 9] = [
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    ];
+
+    finder(line.iter().cloned(), NUMS_FORWARD)
+}
+
+fn first_backward(line: &[u8]) -> (usize, usize) {
+    const NUMS_BACKWARD: [&str; 9] = [
+        "eno", "owt", "eerht", "ruof", "evif", "xis", "neves", "thgie", "enin",
+    ];
+
+    finder(line.iter().rev().cloned(), NUMS_BACKWARD)
+}
+
+fn finder(line: impl Iterator<Item = u8>, searches: [&'static str; 9]) -> (usize, usize) {
+    let mut found_p1 = false;
+    let mut found_p2 = false;
+
+    let mut p1 = 0;
+    let mut p2 = 0;
+
+    let mut incrementors = searches.map(|n| Incrementor {
+        src: n.as_bytes(),
+        index: 0,
+    });
+
+    for b in line {
+        if found_p1 && found_p2 {
+            break;
+        }
+        match b {
+            b'1'..=b'9' => {
+                let num = (b - b'0') as usize;
+                p1 = num;
+                found_p1 = true;
+                if !found_p2 {
+                    p2 = num;
+                    found_p2 = true;
+                }
+            }
+            b => {
+                if found_p2 {
+                    continue;
+                }
+                for (i, incrementor) in incrementors.iter_mut().enumerate() {
+                    if incrementor.feed(b) {
+                        p2 = i + 1;
+                        found_p2 = true;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    (p1, p2)
+}
+
+struct Incrementor {
+    src: &'static [u8],
+    index: usize,
+}
+
+impl Incrementor {
+    fn feed(&mut self, b: u8) -> bool {
+        if self.src[self.index] == b {
+            self.index += 1;
+        } else if self.src[0] == b {
+            self.index = 1;
+        } else {
+            self.index = 0;
+        }
+        self.index == self.src.len()
+    }
 }
 
 #[cfg(test)]
