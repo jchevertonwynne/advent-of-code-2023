2fe6c2bfe72bb95d0a24dee9ca851d431e874243
Joseph Cheverton-Wynne <jchevertonwynne@gmail.com>
2023-12-03 09:52:49 +0000

    day 3

--- src/days/day03.rs
+++ src/days/day03.rs
@@ -1,42 +1,80 @@
+use std::collections::{HashMap, HashSet};
+
+use fxhash::FxBuildHasher;
+use itertools::Itertools;
+
 use crate::{DayResult, IntoDayResult};
 
 pub fn solve(input: &str) -> anyhow::Result<DayResult> {
     let mut p1 = 0;
 
-
     let world = input.lines().map(|l| l.as_bytes()).collect::<Vec<_>>();
 
     let mut building_number = false;
     let mut found_symbol = false;
+    let mut number = 0;
+    let mut number_count = 0;
+    let mut numbers = HashMap::with_hasher(FxBuildHasher::default());
+    let mut asterisks = HashMap::with_hasher(FxBuildHasher::default());
 
     for (y, line) in world.iter().enumerate() {
         for (x, &b) in line.iter().enumerate() {
             if b.is_ascii_digit() {
-                    if !building_number {
-                    found_symbol = true;
+                if !building_number {
+                    number_count += 1;
                 }
-                    building_number = true;
-                    for (i, j) in (-1_isize..=1).zip(-1_isize..=1) {
-                        let nx: usize = match ((x as isize) - i).try_into() {
-                            Ok(nx) => nx,
-                            Err(_) => continue,
-                        };
-                        let ny: usize = match ((y as isize) - j).try_into() {
-                            Ok(ny) => ny,
-                            Err(_) => continue,
-                        };
-                        let t = world[ny][nx];
+                building_number = true;
+                number = number * 10 + (b - b'0') as usize;
+                for (i, j) in (-1_isize..=1).cartesian_product(-1_isize..=1) {
+                    let Ok(nx) = usize::try_from((x as isize) + i) else {
+                        continue;
+                    };
+                    let Ok(ny) = usize::try_from((y as isize) + j) else {
+                        continue;
+                    };
+                    if let Some(&t) = world.get(ny).and_then(|l| l.get(nx)) {
                         if t != b'.' && !t.is_ascii_digit() {
+                            if t == b'*' {
+                                asterisks
+                                    .entry((nx, ny))
+                                    .or_insert(HashSet::with_hasher(FxBuildHasher::default()))
+                                    .insert(number_count);
+                            }
                             found_symbol = true;
                         }
                     }
+                }
             } else {
+                if building_number && found_symbol {
+                    p1 += number;
+                    numbers.insert(number_count, number);
+                }
+                number = 0;
                 building_number = false;
+                found_symbol = false;
             }
         }
+        if building_number && found_symbol {
+            p1 += number;
+            numbers.insert(number_count, number);
+        }
+        number = 0;
+        building_number = false;
+        found_symbol = false;
     }
-    
-    p1.into_result()
+
+    let p2 = asterisks
+        .into_iter()
+        .filter(|(_, numbers)| numbers.len() == 2)
+        .map(|(_, number_counts)| {
+            number_counts
+                .into_iter()
+                .flat_map(|count| numbers.get(&count))
+                .product::<usize>()
+        })
+        .sum::<usize>();
+
+    (p1, p2).into_result()
 }
 
 #[cfg(test)]
@@ -47,19 +85,13 @@ mod tests {
     fn works_for_example() {
         const INPUT: &str = include_str!("../../input/day03_test.txt");
         let solution = solve(INPUT).unwrap();
-        assert_eq!(
-            ().into_day_result(),
-            solution
-        );
+        assert_eq!((4_361, 467_835).into_day_result(), solution);
     }
 
     #[test]
     fn works_for_input() {
         const INPUT: &str = include_str!("../../input/day03.txt");
         let solution = solve(INPUT).unwrap();
-        assert_eq!(
-            ().into_day_result(),
-            solution
-        );
+        assert_eq!((530_849, 84_900_879).into_day_result(), solution);
     }
 }
