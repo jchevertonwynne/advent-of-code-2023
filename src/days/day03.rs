c74e68d609d3a0bb622f290d4a8be82686b57cf7
Joseph Cheverton-Wynne <jchevertonwynne@gmail.com>
2023-12-03 16:20:40 +0000

    performance improvements

--- src/days/day03.rs
+++ src/days/day03.rs
@@ -1,5 +1,7 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 
+use anyhow::Context;
+use arrayvec::ArrayVec;
 use fxhash::FxBuildHasher;
 use itertools::Itertools;
 
@@ -20,7 +22,16 @@ macro_rules! update {
 pub fn solve(input: &str) -> anyhow::Result<DayResult> {
     let mut p1 = 0;
 
-    let world = input.lines().map(|l| l.as_bytes()).collect::<Vec<_>>();
+    let width = input
+        .as_bytes()
+        .iter()
+        .position(|&b| b == b'\n')
+        .context("expected a newline")?;
+
+    let world = World {
+        src: input.as_bytes(),
+        width,
+    };
 
     let mut building_number = false;
     let mut found_symbol = false;
@@ -29,12 +40,11 @@ pub fn solve(input: &str) -> anyhow::Result<DayResult> {
     let mut numbers = HashMap::with_hasher(FxBuildHasher::default());
     let mut asterisks = HashMap::with_hasher(FxBuildHasher::default());
 
-    for (y, line) in world.iter().enumerate() {
-        for (x, &b) in line.iter().enumerate() {
+    for y in 0..(input.len() / (width + 1)) {
+        for x in 0..width {
+            let b = world.get(x, y).context("this is a known legal coord")?;
             if b.is_ascii_digit() {
-                if !building_number {
-                    number_count += 1;
-                }
+                number_count += 1 & (!building_number) as usize;
                 building_number = true;
                 number = number * 10 + (b - b'0') as usize;
                 for (i, j) in (-1_isize..=1).cartesian_product(-1_isize..=1) {
@@ -44,17 +54,19 @@ pub fn solve(input: &str) -> anyhow::Result<DayResult> {
                     let Ok(ny) = usize::try_from((y as isize) + j) else {
                         continue;
                     };
-                    if let Some(&t) = world.get(ny).and_then(|l| l.get(nx)) {
-                        if t != b'.' && !t.is_ascii_digit() {
-                            if t == b'*' {
-                                asterisks
-                                    .entry((nx, ny))
-                                    .or_insert(HashSet::with_hasher(FxBuildHasher::default()))
-                                    .insert(number_count);
-                            }
-                            found_symbol = true;
+                    let Some(t) = world.get(nx, ny) else { continue };
+                    if t == b'.' || t.is_ascii_digit() {
+                        continue;
+                    }
+                    if t == b'*' {
+                        let ids = asterisks
+                            .entry(nx + ny * width)
+                            .or_insert(ArrayVec::<_, 2>::new());
+                        if !ids.contains(&number_count) {
+                            ids.push(number_count);
                         }
                     }
+                    found_symbol = true;
                 }
             } else {
                 update!(
@@ -90,6 +102,20 @@ pub fn solve(input: &str) -> anyhow::Result<DayResult> {
         .sum::<usize>();
 
     (p1, p2).into_result()
+}
+
+struct World<'a> {
+    src: &'a [u8],
+    width: usize,
+}
+
+impl<'a> World<'a> {
+    fn get(&self, w: usize, h: usize) -> Option<u8> {
+        self.src
+            .get(w + h * (self.width + 1))
+            .cloned()
+            .filter(|&b| b != b'\n')
+    }
 }
 
 #[cfg(test)]
