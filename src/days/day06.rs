9fef6ddca0ee48423ba2c91d8215d1b54336b944
Joseph Cheverton-Wynne <jchevertonwynne@gmail.com>
2023-12-06 23:57:04 +0000

    faster day 6 via search

--- src/days/day06.rs
+++ src/days/day06.rs
@@ -6,68 +6,62 @@ use crate::{DayResult, IntoDayResult};
 
 pub fn solve(input: &str) -> anyhow::Result<DayResult> {
     let input = input.as_bytes();
-    let (input, times) = parse_numbers(input)?;
-    let (_, distances) = parse_numbers(input)?;
+    let (input, times, bigger_time) = parse_numbers(input);
+    let (_, distances, bigger_dist) = parse_numbers(input);
 
-    let p1: usize = times
+    let p1: u64 = times
         .iter()
         .zip(distances.iter())
         .map(|(&time, &dist)| race(time, dist))
         .product();
-
-    let bigger_time = combine_numbers(&times);
-    let bigger_dist = combine_numbers(&distances);
 
     let p2 = race(bigger_time, bigger_dist);
 
     (p1, p2).into_result()
 }
 
-fn combine_numbers(nums: &[u64]) -> u64 {
-    nums.iter().fold(0, |mut acc, &n| {
-        let mut t2 = n;
-        while t2 > 0 {
-            acc *= 10;
-            t2 /= 10;
+fn race(time: u64, distance: u64) -> u64 {
+    let mut start = 0;
+    while (start + 1) * (time - (start + 1)) < distance {
+        let mut incr = 1;
+        loop {
+            let curr_start = start + incr;
+
+            if curr_start * (time - curr_start) > distance {
+                break;
+            }
+
+            incr <<= 1;
         }
-        acc + n
-    })
+
+        start += incr >> 1;
+    }
+
+    time - (start + 1) * 2 + 1
 }
 
-fn race(time: u64, distance: u64) -> usize {
-    let time = time as f64;
-    let distance = distance as f64;
-    let inner = ((time * time) - (4.0 * distance)).sqrt();
-    let higher = (time + inner) / 2.0;
-    let lower = (time - inner) / 2.0;
-    let higher = if higher % 1.0 == 0.0 {
-        higher - 1.0
-    } else {
-        higher
-    };
-    let lower = if lower % 1.0 == 0.0 {
-        lower + 1.0
-    } else {
-        lower
-    };
-
-    higher.floor() as usize - lower.ceil() as usize + 1
-}
-
-fn parse_numbers(mut input: &[u8]) -> Result<(&[u8], ArrayVec<u64, 4>), ParseIntError> {
+fn parse_numbers(mut input: &[u8]) -> (&[u8], ArrayVec<u64, 4>, u64) {
     let mut res = ArrayVec::new();
+    let mut big_num = 0;
+
     while input[0] != b'\n' {
         while !input[0].is_ascii_digit() {
             input = &input[1..];
         }
+
         let mut num = 0;
+
         while input[0].is_ascii_digit() {
-            num = num * 10 + (input[0] - b'0') as u64;
+            let digit = (input[0] - b'0') as u64;
+            num = num * 10 + digit;
+            big_num = big_num * 10 + digit;
             input = &input[1..];
         }
+
         res.push(num);
     }
-    Ok((&input[1..], res))
+
+    (&input[1..], res, big_num)
 }
 
 #[cfg(test)]
