d9016aad629ffb75354869486b34911af79fd45f
Joseph Cheverton-Wynne <jchevertonwynne@gmail.com>
2023-12-08 18:48:38 +0000

    bump allocator

--- src/days/day08.rs
+++ src/days/day08.rs
@@ -1,4 +1,7 @@
-use std::collections::hash_map::Entry;
+use std::{
+    cell::Cell,
+    collections::{hash_map::Entry, VecDeque},
+};
 
 use anyhow::Context;
 use fxhash::FxHashMap;
@@ -11,36 +14,69 @@ pub fn solve(input: &str, is_test: bool) -> anyhow::Result<DayResult> {
     let instructions = it.next().context("should have first line")?;
     let mut input = &input[instructions.len() + 2..];
 
-    let mut map = fxhash::FxHashMap::default();
+    let mut raw_nodes = VecDeque::with_capacity(input.len() / 17);
 
     while !input.is_empty() {
-        let (n, node) = parse_line(input);
+        let raw_node = NodeRaw {
+            val: &input[..3],
+            left: &input[7..10],
+            right: &input[12..15],
+        };
+
         input = &input[17..];
-        map.insert(n, node);
+        raw_nodes.push_back(raw_node);
+    }
+
+    let b = bumpalo::Bump::new();
+
+    let mut nodes = FxHashMap::default();
+    while let Some(raw_node) = raw_nodes.pop_front() {
+        let real_node = nodes.entry(raw_node.val).or_insert_with(|| {
+            b.alloc(Node {
+                val: raw_node.val,
+                left: Cell::new(std::ptr::null()),
+                right: Cell::new(std::ptr::null()),
+            }) as &Node
+        }) as &Node;
+        if real_node.left.get().is_null() {
+            let Some(left) = nodes.get(raw_node.left) else {
+                raw_nodes.push_back(raw_node);
+                continue;
+            };
+            real_node.left.set(*left);
+        }
+        if real_node.right.get().is_null() {
+            let Some(right) = nodes.get(raw_node.right) else {
+                raw_nodes.push_back(raw_node);
+                continue;
+            };
+            real_node.right.set(*right);
+        }
     }
 
     let mut p1 = 0;
     if !is_test {
         let instructions_iter = instructions.iter().cycle();
-        let mut curr = if is_test { b"11A" } else { b"AAA" }.as_slice();
+        let mut curr = nodes[b"AAA".as_slice()];
+        let end = nodes[b"ZZZ".as_slice()];
         for &instruction in instructions_iter {
             if instruction == b'L' {
-                curr = map[curr].left;
+                curr = unsafe { &*curr.left.get() };
             } else {
-                curr = map[curr].right;
+                curr = unsafe { &*curr.right.get() };
             }
             p1 += 1;
-            if curr == b"ZZZ" {
+            if std::ptr::eq(curr, end) {
                 break;
             }
         }
     }
 
     let mut cache = FxHashMap::default();
-    let p2 = map
-        .keys()
-        .filter_map(|k| if k.ends_with(b"A") { Some(*k) } else { None })
-        .map(|k| to_z_loop(instructions, k, &map, &mut cache))
+    let p2 = nodes
+        .iter()
+        .filter_map(|(k, v)| if k.ends_with(b"A") { Some(*v) } else { None })
+        .map(|v| to_z_loop(instructions, v, &mut cache))
         .reduce(num::integer::lcm)
         .context("iter has 1+ elements")?;
 
@@ -49,16 +85,15 @@ pub fn solve(input: &str, is_test: bool) -> anyhow::Result<DayResult> {
 
 fn to_z_loop<'a>(
     instructions: &'a [u8],
-    start: &'a [u8],
-    map: &FxHashMap<&'a [u8], Node<'a>>,
-    cache: &mut FxHashMap<&'a [u8], usize>,
+    start: &'a Node<'a>,
+    cache: &mut FxHashMap<*const Node<'a>, usize>,
 ) -> i128 {
     cache.clear();
     let instructions_iter = instructions.iter().cycle();
     let mut curr = start;
     for (i, &instruction) in instructions_iter.enumerate() {
-        if curr[2] == b'Z' {
-            match cache.entry(curr) {
+        if curr.val[2] == b'Z' {
+            match cache.entry(curr as *const _) {
                 Entry::Occupied(entry) => {
                     let last_seen_at = *entry.get();
                     return last_seen_at as i128;
@@ -66,30 +101,28 @@ fn to_z_loop<'a>(
                 Entry::Vacant(v) => v.insert(i),
             };
         }
-        curr = if instruction == b'L' {
-            map[curr].left
+        if instruction == b'L' {
+            curr = unsafe { &*curr.left.get() };
         } else {
-            map[curr].right
+            curr = unsafe { &*curr.right.get() };
         };
     }
 
     unreachable!("lmao")
 }
 
-fn parse_line(line: &[u8]) -> (&[u8], Node) {
-    (
-        &line[..3],
-        Node {
-            left: &line[7..10],
-            right: &line[12..15],
-        },
-    )
-}
-
-#[derive(Debug, Clone, Copy)]
-struct Node<'a> {
+#[derive(Debug)]
+struct NodeRaw<'a> {
+    val: &'a [u8],
     left: &'a [u8],
     right: &'a [u8],
+}
+
+#[derive(Debug)]
+struct Node<'a> {
+    val: &'a [u8],
+    left: Cell<*const Node<'a>>,
+    right: Cell<*const Node<'a>>,
 }
 
 #[cfg(test)]
