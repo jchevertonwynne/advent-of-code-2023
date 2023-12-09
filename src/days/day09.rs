858b3bab86981e8197480ab08e1714523d79cdaa
Joseph Cheverton-Wynne <jchevertonwynne@gmail.com>
2023-12-09 13:37:59 +0000

    mid day 9 refactor

--- src/days/day09.rs
+++ src/days/day09.rs
@@ -4,19 +4,16 @@ use itertools::Itertools;
 use crate::{DayResult, IntoDayResult};
 
 pub fn solve(input: &str) -> anyhow::Result<DayResult> {
-    let pool = object_pool::Pool::new(4, Vec::<isize>::new);
-
     let mut p1 = 0;
     let mut p2 = 0;
 
     let mut triangle = Vec::new();
+    let mut layers = 0;
 
     let mut input = input.as_bytes();
 
     while !input.is_empty() {
         triangle.clear();
-        let mut vd = pool.pull(Vec::new);
-        vd.clear();
 
         while !input.is_empty() {
             let mut negative = false;
@@ -35,40 +32,40 @@ pub fn solve(input: &str) -> anyhow::Result<DayResult> {
             if negative {
                 n *= -1;
             }
-            vd.push(n);
+            triangle.push(n);
             let last = input[0];
             input = &input[1..];
             if last == b'\n' {
                 break;
             }
         }
-        triangle.push(vd);
+        layers = 1;
+
         loop {
-            let last = triangle.last().context("exp at least 1 row")?;
             let mut final_row = true;
-            let mut vd = pool.pull(Vec::new);
-            vd.clear();
-            let next = last
-                .iter()
-                .tuple_windows()
-                .map(|(&a, &b)| {
-                    let res = b - a;
-                    final_row &= res == 0;
-                    res
-                })
-                .fold(vd, |mut acc, v| {
-                    acc.push(v);
-                    acc
-                });
-            triangle.push(next);
+            let r = range(layers, layers, triangle.len());
+            for a in r.start..(r.end - 1) {
+                let val = triangle[a + 1] - triangle[a];
+                final_row &= val == 0;
+                triangle.push(val);
+            }
+            layers += 1;
             if final_row {
                 break;
             }
         }
 
-        let (p1_end, p2_front) = triangle.iter().rev().fold((0, 0), |(p1_end, p2_front), t| {
-            (t[t.len() - 1] + p1_end, t[0] - p2_front)
+        let mut end = 0;
+        let (p1_end, p2_front) = (1..=layers).fold((0, 0), |(p1_end, p2_front), layer| {
+            let r = range(layers, layer, triangle.len());
+            assert_eq!(end, r.start);
+            end = r.end;
+            let row = &triangle[r];
+            println!("{:?} {}", row, row.len());
+            (row[row.len() - 1] + p1_end, row[0] - p2_front)
         });
+
+        println!("1= {} 2= {}", p1_end, p2_front);
         p1 += p1_end;
         p2 += p2_front;
     }
@@ -76,6 +73,20 @@ pub fn solve(input: &str) -> anyhow::Result<DayResult> {
     (p1, p2).into_result()
 }
 
+fn triangle(n: usize) -> usize {
+    if n == 0 {
+        return 0;
+    }
+    (n * (n - 1)) / 2
+}
+
+fn range(layers: usize, row: usize, len: usize) -> std::ops::Range<usize> {
+    let t = triangle(layers);
+    let common = (len - t) / layers;
+    let extra = layers - row;
+    let start = common * (row - 1) + (t - triangle(layers - row + 1));
+    start..(start + common + extra)
+}
 #[cfg(test)]
 mod tests {
     use crate::{days::day09::solve, IntoDayResult};
