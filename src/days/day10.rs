c33bab96df27d6fe64e9c090c4b8be759c1a3c89
Joseph Cheverton-Wynne <jchevertonwynne@gmail.com>
2023-12-10 11:47:19 +0000

    made code less horrendous

--- src/days/day10.rs
+++ src/days/day10.rs
@@ -1,3 +1,5 @@
+use std::ops::ControlFlow;
+
 use anyhow::Context;
 use fxhash::FxHashSet;
 use strum::EnumIter;
@@ -45,87 +47,19 @@ pub fn solve(input: &str) -> anyhow::Result<DayResult> {
             };
 
             let next_tile = world.get(nx, ny);
-            match next_tile {
-                Tile::Vertical => {
-                    if matches!(dir, Direction::Up | Direction::Down) {
-                    } else {
-                        break;
-                    }
-                }
-                Tile::Horizontal => {
-                    if matches!(dir, Direction::Left | Direction::Right) {
-                    } else {
-                        break;
-                    }
-                }
-                Tile::CornerL => {
-                    if matches!(dir, Direction::Left | Direction::Down) {
-                        dir = if dir == Direction::Left {
-                            Direction::Up
-                        } else {
-                            Direction::Right
-                        }
-                    } else {
-                        break;
-                    }
-                }
-                Tile::CornerJ => {
-                    if matches!(dir, Direction::Right | Direction::Down) {
-                        dir = if dir == Direction::Right {
-                            Direction::Up
-                        } else {
-                            Direction::Left
-                        }
-                    } else {
-                        break;
-                    }
-                }
-                Tile::Corner7 => {
-                    if matches!(dir, Direction::Right | Direction::Up) {
-                        dir = if dir == Direction::Right {
-                            Direction::Down
-                        } else {
-                            Direction::Left
-                        }
-                    } else {
-                        break;
-                    }
-                }
-                Tile::CornerF => {
-                    if matches!(dir, Direction::Left | Direction::Up) {
-                        dir = if dir == Direction::Left {
-                            Direction::Down
-                        } else {
-                            Direction::Right
-                        }
-                    } else {
-                        break;
-                    }
-                }
-                Tile::Start => {
-                    start_tile = match (start_dir, dir) {
-                        (Direction::Up, Direction::Up) => Tile::Vertical,
-                        (Direction::Up, Direction::Left) => Tile::CornerL,
-                        (Direction::Up, Direction::Right) => Tile::CornerJ,
-
-                        (Direction::Down, Direction::Down) => Tile::Vertical,
-                        (Direction::Down, Direction::Left) => Tile::CornerF,
-                        (Direction::Down, Direction::Right) => Tile::Corner7,
-
-                        (Direction::Left, Direction::Up) => Tile::Corner7,
-                        (Direction::Left, Direction::Down) => Tile::CornerJ,
-                        (Direction::Left, Direction::Left) => Tile::Horizontal,
-
-                        (Direction::Right, Direction::Up) => Tile::CornerF,
-                        (Direction::Right, Direction::Down) => Tile::CornerL,
-                        (Direction::Right, Direction::Right) => Tile::Horizontal,
-                        _ => unreachable!("lmao"),
-                    };
-                    moves += 1;
-                    p1 = moves / 2;
+            if let ControlFlow::Break(break_inner) = process_tile(
+                next_tile,
+                &mut dir,
+                &mut start_tile,
+                start_dir,
+                &mut moves,
+                &mut p1,
+            ) {
+                if break_inner {
+                    break;
+                } else {
                     break 'd;
                 }
-                Tile::Empty => break,
             }
 
             x = nx;
@@ -153,153 +87,18 @@ pub fn solve(input: &str) -> anyhow::Result<DayResult> {
     // we can only be on a vertival or a left corner
     // if we're on a vertical tile - read to right
     // else if corner move right/up
-    let mut dir = match tile {
-        Tile::Vertical => Direction::Up,
-        Tile::CornerL => Direction::Left,
-        Tile::CornerF => Direction::Up,
-        Tile::Start => match start_tile {
-            Tile::Vertical => Direction::Up,
-            Tile::CornerL => Direction::Left,
-            Tile::CornerF => Direction::Up,
-            _ => unreachable!("found a {tile:?} at {x} {y}"),
-        },
-        _ => unreachable!("found a {tile:?} at {x} {y}"),
-    };
+    let mut dir = find_start_dir(tile, start_tile);
     loop {
         let tile = world.get(x, y);
-        match tile {
-            Tile::Vertical => {
-                if dir == Direction::Up {
-                    flood_fill(x + 1, y, &pipe_tiles, &mut contained);
-                    y -= 1;
-                } else {
-                    flood_fill(x - 1, y, &pipe_tiles, &mut contained);
-                    y += 1;
-                }
-            }
-            Tile::Horizontal => {
-                if dir == Direction::Right {
-                    flood_fill(x, y + 1, &pipe_tiles, &mut contained);
-                    x += 1;
-                } else {
-                    flood_fill(x, y - 1, &pipe_tiles, &mut contained);
-                    x -= 1;
-                }
-            }
-            Tile::Corner7 => {
-                if dir == Direction::Up {
-                    flood_fill(x, y - 1, &pipe_tiles, &mut contained);
-                    flood_fill(x + 1, y, &pipe_tiles, &mut contained);
-                    dir = Direction::Left;
-                    x -= 1;
-                } else {
-                    dir = Direction::Down;
-                    y += 1;
-                }
-            }
-            Tile::CornerJ => {
-                if dir == Direction::Down {
-                    dir = Direction::Left;
-                    x -= 1;
-                } else {
-                    flood_fill(x, y + 1, &pipe_tiles, &mut contained);
-                    flood_fill(x + 1, y, &pipe_tiles, &mut contained);
-                    dir = Direction::Up;
-                    y -= 1;
-                }
-            }
-            Tile::CornerL => {
-                if dir == Direction::Down {
-                    flood_fill(x, y + 1, &pipe_tiles, &mut contained);
-                    flood_fill(x - 1, y, &pipe_tiles, &mut contained);
-                    dir = Direction::Right;
-                    x += 1;
-                } else {
-                    dir = Direction::Up;
-                    y -= 1;
-                }
-            }
-            Tile::CornerF => {
-                if dir == Direction::Up {
-                    dir = Direction::Right;
-                    x += 1;
-                } else {
-                    flood_fill(x, y - 1, &pipe_tiles, &mut contained);
-                    flood_fill(x - 1, y, &pipe_tiles, &mut contained);
-                    dir = Direction::Down;
-                    y += 1;
-                }
-            }
-            Tile::Start => {
-                match start_tile {
-                    Tile::Vertical => {
-                        if dir == Direction::Up {
-                            flood_fill(x + 1, y, &pipe_tiles, &mut contained);
-                            y -= 1;
-                            // to right
-                        } else {
-                            flood_fill(x - 1, y, &pipe_tiles, &mut contained);
-                            y += 1;
-                        }
-                    }
-                    Tile::Horizontal => {
-                        if dir == Direction::Right {
-                            flood_fill(x, y + 1, &pipe_tiles, &mut contained);
-                            x += 1;
-                        } else {
-                            flood_fill(x, y - 1, &pipe_tiles, &mut contained);
-                            x -= 1;
-                        }
-                    }
-                    Tile::Corner7 => {
-                        if dir == Direction::Up {
-                            flood_fill(x, y - 1, &pipe_tiles, &mut contained);
-                            flood_fill(x + 1, y, &pipe_tiles, &mut contained);
-                            dir = Direction::Left;
-                            x -= 1;
-                        } else {
-                            dir = Direction::Down;
-                            y += 1;
-                        }
-                    }
-                    Tile::CornerJ => {
-                        if dir == Direction::Down {
-                            dir = Direction::Left;
-                            x -= 1;
-                        } else {
-                            flood_fill(x, y + 1, &pipe_tiles, &mut contained);
-                            flood_fill(x + 1, y, &pipe_tiles, &mut contained);
-                            dir = Direction::Up;
-                            y -= 1;
-                        }
-                    }
-                    Tile::CornerL => {
-                        if dir == Direction::Down {
-                            flood_fill(x, y + 1, &pipe_tiles, &mut contained);
-                            flood_fill(x - 1, y, &pipe_tiles, &mut contained);
-                            dir = Direction::Right;
-                            x += 1;
-                        } else {
-                            dir = Direction::Up;
-                            y -= 1;
-                        }
-                    }
-                    Tile::CornerF => {
-                        if dir == Direction::Up {
-                            dir = Direction::Right;
-                            x += 1;
-                        } else {
-                            flood_fill(x, y - 1, &pipe_tiles, &mut contained);
-                            flood_fill(x - 1, y, &pipe_tiles, &mut contained);
-                            dir = Direction::Down;
-                            y += 1;
-                        }
-                    }
-                    _ => unreachable!("found a {tile:?} at {x} {y}"),
-                }
-            }
-            _ => unreachable!("found a {tile:?} at {x} {y}"),
-        };
+        follow_pipe_and_flood_fill(
+            tile,
+            &mut dir,
+            &mut x,
+            &mut y,
+            &pipe_tiles,
+            &mut contained,
+            start_tile,
+        );
 
         if (x, y) == new_start {
             break;
@@ -309,6 +108,192 @@ pub fn solve(input: &str) -> anyhow::Result<DayResult> {
     let p2 = contained.len();
 
     (p1, p2).into_result()
+}
+
+fn process_tile(
+    next_tile: Tile,
+    dir: &mut Direction,
+    start_tile: &mut Tile,
+    start_dir: Direction,
+    moves: &mut i32,
+    p1: &mut i32,
+) -> ControlFlow<bool> {
+    match next_tile {
+        Tile::Vertical => {
+            if matches!(*dir, Direction::Up | Direction::Down) {
+            } else {
+                return ControlFlow::Break(true);
+            }
+        }
+        Tile::Horizontal => {
+            if matches!(*dir, Direction::Left | Direction::Right) {
+            } else {
+                return ControlFlow::Break(true);
+            }
+        }
+        Tile::CornerL => {
+            if matches!(*dir, Direction::Left | Direction::Down) {
+                *dir = if *dir == Direction::Left {
+                    Direction::Up
+                } else {
+                    Direction::Right
+                }
+            } else {
+                return ControlFlow::Break(true);
+            }
+        }
+        Tile::CornerJ => {
+            if matches!(*dir, Direction::Right | Direction::Down) {
+                *dir = if *dir == Direction::Right {
+                    Direction::Up
+                } else {
+                    Direction::Left
+                }
+            } else {
+                return ControlFlow::Break(true);
+            }
+        }
+        Tile::Corner7 => {
+            if matches!(*dir, Direction::Right | Direction::Up) {
+                *dir = if *dir == Direction::Right {
+                    Direction::Down
+                } else {
+                    Direction::Left
+                }
+            } else {
+                return ControlFlow::Break(true);
+            }
+        }
+        Tile::CornerF => {
+            if matches!(*dir, Direction::Left | Direction::Up) {
+                *dir = if *dir == Direction::Left {
+                    Direction::Down
+                } else {
+                    Direction::Right
+                }
+            } else {
+                return ControlFlow::Break(true);
+            }
+        }
+        Tile::Start => {
+            *start_tile = find_pipe_type(start_dir, *dir);
+            *moves += 1;
+            *p1 = *moves / 2;
+            return ControlFlow::Break(false);
+        }
+        Tile::Empty => return ControlFlow::Break(true),
+    }
+    ControlFlow::Continue(())
+}
+
+fn find_pipe_type(start_dir: Direction, dir: Direction) -> Tile {
+    match (start_dir, dir) {
+        (Direction::Up, Direction::Up) => Tile::Vertical,
+        (Direction::Up, Direction::Left) => Tile::CornerL,
+        (Direction::Up, Direction::Right) => Tile::CornerJ,
+
+        (Direction::Down, Direction::Down) => Tile::Vertical,
+        (Direction::Down, Direction::Left) => Tile::CornerF,
+        (Direction::Down, Direction::Right) => Tile::Corner7,
+
+        (Direction::Left, Direction::Up) => Tile::Corner7,
+        (Direction::Left, Direction::Down) => Tile::CornerJ,
+        (Direction::Left, Direction::Left) => Tile::Horizontal,
+
+        (Direction::Right, Direction::Up) => Tile::CornerF,
+        (Direction::Right, Direction::Down) => Tile::CornerL,
+        (Direction::Right, Direction::Right) => Tile::Horizontal,
+        _ => unreachable!("lmao"),
+    }
+}
+
+fn follow_pipe_and_flood_fill(
+    tile: Tile,
+    dir: &mut Direction,
+    x: &mut usize,
+    y: &mut usize,
+    pipe_tiles: &FxHashSet<(usize, usize)>,
+    contained: &mut FxHashSet<(usize, usize)>,
+    start_tile: Tile,
+) {
+    match tile {
+        Tile::Vertical => {
+            if *dir == Direction::Up {
+                flood_fill(*x + 1, *y, pipe_tiles, contained);
+                *y -= 1;
+            } else {
+                flood_fill(*x - 1, *y, pipe_tiles, contained);
+                *y += 1;
+            }
+        }
+        Tile::Horizontal => {
+            if *dir == Direction::Right {
+                flood_fill(*x, *y + 1, pipe_tiles, contained);
+                *x += 1;
+            } else {
+                flood_fill(*x, *y - 1, pipe_tiles, contained);
+                *x -= 1;
+            }
+        }
+        Tile::Corner7 => {
+            if *dir == Direction::Up {
+                flood_fill(*x, *y - 1, pipe_tiles, contained);
+                flood_fill(*x + 1, *y, pipe_tiles, contained);
+                *dir = Direction::Left;
+                *x -= 1;
+            } else {
+                *dir = Direction::Down;
+                *y += 1;
+            }
+        }
+        Tile::CornerJ => {
+            if *dir == Direction::Down {
+                *dir = Direction::Left;
+                *x -= 1;
+            } else {
+                flood_fill(*x, *y + 1, pipe_tiles, contained);
+                flood_fill(*x + 1, *y, pipe_tiles, contained);
+                *dir = Direction::Up;
+                *y -= 1;
+            }
+        }
+        Tile::CornerL => {
+            if *dir == Direction::Down {
+                flood_fill(*x, *y + 1, pipe_tiles, contained);
+                flood_fill(*x - 1, *y, pipe_tiles, contained);
+                *dir = Direction::Right;
+                *x += 1;
+            } else {
+                *dir = Direction::Up;
+                *y -= 1;
+            }
+        }
+        Tile::CornerF => {
+            if *dir == Direction::Up {
+                *dir = Direction::Right;
+                *x += 1;
+            } else {
+                flood_fill(*x, *y - 1, pipe_tiles, contained);
+                flood_fill(*x - 1, *y, pipe_tiles, contained);
+                *dir = Direction::Down;
+                *y += 1;
+            }
+        }
+        Tile::Start => {
+            follow_pipe_and_flood_fill(start_tile, dir, x, y, pipe_tiles, contained, start_tile)
+        }
+        _ => unreachable!("found a {tile:?} at {x} {y}"),
+    };
+}
+
+fn find_start_dir(tile: Tile, start_tile: Tile) -> Direction {
+    match tile {
+        Tile::Vertical => Direction::Up,
+        Tile::CornerL => Direction::Left,
+        Tile::CornerF => Direction::Up,
+        Tile::Start => find_start_dir(start_tile, start_tile),
+        _ => unreachable!("found a {tile:?}"),
+    }
 }
 
 fn flood_fill(
@@ -336,7 +321,7 @@ enum Direction {
     Right,
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone, Copy)]
 enum Tile {
     Vertical,
     Horizontal,
